f83249990b2f2b28dede6dcd6a5264dad1029f04
masrul <mail2masrul@gmail.com>
2019-08-16 18:17:29 -0500

    add dumping differences

--- cooling-1-source.py
+++ cooling-1-source.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation 
 n=75  # grid size
 r=10  # source size 
-T=500 # temperature 
+T=1000 # temperature 
 
-nframes=1000
+niterations=10000 
+dframes=5 # dumping inverval of frames  
 duration=20 #sec 
 
 room=np.zeros((n, n))
@@ -37,9 +38,10 @@ plt.tick_params(
     labelleft=False) 
 
 ims=[]
+nframes=niterations/dframes 
 fps=nframes/duration
-interval=(duration*1000)/nframes 
-for itr in range(nframes):
+interval=(duration*1000)/nframes  
+for itr in range(niterations):
     print ('iteration# :', itr)   
     for i in range(1,n-1):
         for j in range(1,n-1):
@@ -53,17 +55,22 @@ for itr in range(nframes):
                 room[i+1,j+1]+
                 room[i+1,j-1]
             )
+
+    if((itr%dframes)==0):
+        print("Dumping itration#:",itr)
+        print() 
     
-    im = plt.imshow(
-            room, 
-            interpolation='none', 
-            cmap=cm.Spectral_r,
-            origin='lower', 
-            extent=[-10,10,-10,10],
-            vmax=100, vmin=0
-        )
-    ims.append([im])
+        im = plt.imshow(
+                room, 
+                interpolation='none', 
+                cmap=cm.Spectral_r,
+                origin='lower', 
+                extent=[-10,10,-10,10],
+                vmax=100, vmin=0
+            )
+        ims.append([im])
 
 ani = animation.ArtistAnimation(fig,ims,interval=interval)
 ani.save('cooling-1-source.gif',fps=fps, dpi=100,writer='imagemagick')
+ani.save('cooling-1-source.mp4',fps=fps, dpi=100)
 plt.show()
--- cooling-4-source.py
+++ cooling-4-source.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation 
-n=150
-r=5    
-T=500
 
-nframes=3000  
+n=150
+r=10     
+T=1000
+
+niterations=10000 
+dframes=5 # dumping inverval of frames  
 duration=20 #sec 
 
 room=np.zeros((n, n))
@@ -65,9 +67,11 @@ plt.tick_params(
     labelleft=False) 
 
 ims=[]
+nframes=niterations/dframes 
 fps=nframes/duration
-interval=(duration*1000)/nframes 
-for itr in range(nframes):
+interval=(duration*1000)/nframes  
+
+for itr in range(niterations):
     print ('iteration# :', itr)  
     for i in range(1,n-1):
         for j in range(1,n-1):
@@ -82,17 +86,20 @@ for itr in range(nframes):
                 room[i+1,j-1]
             )
     
-    im = plt.imshow(
-            room, 
-            interpolation='none', 
-            #cmap=cm.RdYlBu_r,
-            cmap=cm.Spectral_r,
-            origin='lower', 
-            extent=[-10,10,-10,10],
-            vmax=100, vmin=0
-        )
-    ims.append([im])
+    if((itr%dframes)==0):
+        print("Dumping itration#:",itr)
+        print() 
+        im = plt.imshow(
+                room, 
+                interpolation='none', 
+                cmap=cm.Spectral_r,
+                origin='lower', 
+                extent=[-10,10,-10,10],
+                vmax=100, vmin=0
+            )
+        ims.append([im])
 
 ani = animation.ArtistAnimation(fig,ims,interval=interval)
 ani.save('cooling-4-source.gif',fps=fps, dpi=100,writer='imagemagick')
+ani.save('cooling-4-source.mp4',fps=fps, dpi=100)
 plt.show()
